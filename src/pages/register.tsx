3b96e503f451b6b24124fec57ebbe9f029a0aaa6
eliezer-rodrigues037 <eliezer_gt.02@hotmail.com>
2022-09-27 14:49:10 -0300

    Forgot Password ✓

---
+++ src/pages/register.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from "next";
+import { Box, Flex, Text } from "@chakra-ui/react";
+import Head from "next/head";
+const Register: NextPage = () => {
+  return <>Register</>;
+};
+
+export default Register;
